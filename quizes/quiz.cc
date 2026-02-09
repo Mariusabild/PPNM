929a25538ac219a7a354e9cd5edd42e4c22764eb
mariusabild <mariusab49@icloud.com>
2026-02-09 16:58:11 +0100

    exercise

---
+++ quizes/quiz.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+    //for loop type
+    for(int i = 0; i < 4; i++){
+        std::cout << i << "\n";
+    }
+
+    //while loop type
+    int u = 0;
+    while(u < 4){
+        std::cout << u << "\n";
+        u++;
+    }
+    int i = 0;
+    do{
+    std::cout << i << "\n";
+    i++;
+    }while(i <4);
+
+
+    if(i<4){
+        std::cout << i << "\n";
+    } else{
+        std::cout << " i er for stor" << "\n";
+}
+}
+
